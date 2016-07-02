3b7f1d0c9696ad09f422b96acbc426c2fce119e3
Joshua Koo <zz85nus@gmail.com>
2016-07-03 00:08:41 +0800

    More refactoring

--- example.js
+++ example.js
@@ -1,3 +1,5 @@
+const THRESHOLD = 0.7;
+const POLL = 30;
 
 let canvas, ctx;
 
@@ -16,7 +18,7 @@ function loadVideo() {
 		console.log('oncanplay', video.clientWidth, video.clientHeight);
 		canvas.width = video.clientWidth;
 		canvas.height = video.clientHeight;
-		setInterval(process, 100);
+		setInterval( extract, POLL );
 	}
 
 	document.body.appendChild(video);
@@ -37,113 +39,43 @@ function loadImage() {
 		canvas.width = image.width;
 		canvas.height = image.height;
 
-		ctx.drawImage( image, 0, 0 );
-		ctx.strokeStyle = 'yellow';
-		ctx.fillStyle = 'red';
-
-		ctx.beginPath();
-		let midWidth = image.width / 2 | 0;
-		let height = image.height;
-		ctx.rect(midWidth - 2, 0, 3, height);
-		ctx.stroke();
-
-		let idata = ctx.getImageData( 0, 0, image.width, image.height );
-		let data = idata.data;
-
-		let width = image.width;
-		let pixels = image.width * image.height;
-
-		// conversion to greyscale
-		greyScale = new Float32Array( pixels );
-		for ( let i = 0; i < pixels; i++ ) {
-			let ref = i * 4;
-			let r = data[ ref + 0 ] / 255;
-			let g = data[ ref + 1 ] / 255;
-			let b = data[ ref + 2 ] / 255;
-
-			// Y'=0.299R'+0.587G'+0.114B'
-			greyScale[ i ] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-		}
-
-		const THRESHOLD = 0.5;
-
-		dark = new Array(height);
-
-		// apply threshold
-		for ( let i = 0; i < height; i ++ ) {
-			dark[i] = greyScale[ i * width + midWidth ] < THRESHOLD;
-
-			if (dark[i]) {
-				ctx.beginPath();
-				ctx.rect(midWidth - 10, i, 5, 1);
-				ctx.fill();
-			}
-		}
-
-		// console.log( dark );
-
-		// process bits
-		let ok = PhonoPaper.processStrip( dark );
-		// console.log('ok', ok);
-
-		// PhonoPaper.processImage( greyScale );
+		process( image );
 
 	}
 }
 
-function process() {
+function extract() {
+	process( video );
+}
+
+function process( target ) {
 
 	let width = canvas.width;
 	let height = canvas.height;
 	let pixels = width * height;
 
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, width, height);
 
-	ctx.drawImage( video, 0, 0 );
+	ctx.drawImage( target, 0, 0 );
 	ctx.strokeStyle = 'yellow';
 	ctx.fillStyle = 'red';
 
 	ctx.beginPath();
-	let midWidth = width / 2 | 0;
+	const midWidth = width / 2 | 0;
 	ctx.rect(midWidth - 2, 0, 3, height);
 	ctx.stroke();
 
-	let idata = ctx.getImageData( 0, 0, width, height );
-	let data = idata.data;
-
+	idata = ctx.getImageData( 0, 0, width, height );
 
 	// conversion to greyscale
-	greyScale = new Float32Array( pixels );
-	for ( let i = 0; i < pixels; i++ ) {
-		let ref = i * 4;
-		let r = data[ ref + 0 ] / 255;
-		let g = data[ ref + 1 ] / 255;
-		let b = data[ ref + 2 ] / 255;
-
-		// Y'=0.299R'+0.587G'+0.114B'
-		greyScale[ i ] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-	}
-
-	const THRESHOLD = 0.7;
-
-	dark = new Array( height );
+	const greyscale = PhonoPaper.greyscaleImage( idata );
 
 	// apply threshold
-	for ( let i = 0; i < height; i ++ ) {
-		dark[i] = greyScale[ i * width + midWidth ] < THRESHOLD;
-
-		if (dark[i]) {
-			ctx.beginPath();
-			ctx.rect(midWidth - 10, i, 5, 1);
-			ctx.fill();
-		}
-	}
-
-	// console.log( dark );
+	const dark = PhonoPaper.thresholdImage( greyscale, width, height, THRESHOLD );
 
 	// process bits
-	let ok = PhonoPaper.processStrip( dark );
+	const ok = PhonoPaper.processStrip( dark );
 	makeSomeNoise(notesOn);
 	// console.log('ok', ok);
 }
@@ -164,9 +96,8 @@ function loadUserMedia() {
 		// console.log('oncanplay', video.clientWidth, video.clientHeight);
 		canvas.width = video.clientWidth;
 		canvas.height = video.clientHeight;
-		setInterval(process, 25);
+		setInterval(extract, POLL);
 	}
-
 
 	navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
